2bb732f1843266cac5160fc695f587382d9192f3
Someone000111 <maxim0_0_7@mail.ru>
2022-08-27 23:53:55 +0300

    23.53

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 // Define the query
-const mediaQuery = window.matchMedia('(min-width: 768px)')
-
+const mediaQuery = window.matchMedia("(min-width: 768px)");
 
 // Войти или зарег.querySelector(".formInner");
 
@@ -185,8 +184,6 @@ function scroll(selfHeight, selfY) {
     $(window).on("scroll", (windowScrollPosition, hrefNow, log) => {
         hrefNow = document.getElementById("hrefA").href;
 
-     
-
         console.log(hrefNow);
 
         windowScrollPosition = $(window).scrollTop();
@@ -200,7 +197,8 @@ function scroll(selfHeight, selfY) {
             hrefNow ==
                 "file:///C:/Users/Someone/Documents/GitHub/-Kodland-2/index.html#t0" ||
             hrefNow == "http://127.0.0.1:5500/#t0" ||
-            hrefNow == 'file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t0'
+            hrefNow ==
+                "file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t0"
         ) {
             anime({
                 targets: ".advArt",
@@ -217,9 +215,9 @@ function scroll(selfHeight, selfY) {
                     "file:///C:/Users/User/Desktop/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82%20Kodland%202/index.html#t1") ||
             hrefNow ==
                 "file:///C:/Users/Someone/Documents/GitHub/-Kodland-2/index.html#t1" ||
-            hrefNow == "http://127.0.0.1:5500/#t1"||
-            hrefNow == 'file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t1'
-
+            hrefNow == "http://127.0.0.1:5500/#t1" ||
+            hrefNow ==
+                "file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t1"
         ) {
             anime({
                 targets: ".online *",
@@ -237,9 +235,9 @@ function scroll(selfHeight, selfY) {
                     "file:///C:/Users/User/Desktop/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82%20Kodland%202/index.html#t2") ||
             hrefNow ==
                 "file:///C:/Users/Someone/Documents/GitHub/-Kodland-2/index.html#t2" ||
-            hrefNow == "http://127.0.0.1:5500/#t2"||
-            hrefNow == 'file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t2'
-
+            hrefNow == "http://127.0.0.1:5500/#t2" ||
+            hrefNow ==
+                "file:///C:/Users/Someone/Documents/GitHub/My-Kodland-project-2/index.html#t2"
         ) {
             anime({
                 targets: ".years",
